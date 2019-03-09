43a223a14cd594deede578582eae673dcfead1af
Ryan Falzon <musicfreakzryan@hotmail.com>
2019-03-09 14:16:27 +0100

    Working on beneficiery front end application

---
+++ Templates/src/scripts/Beneficiery/Beneficiery.Module.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+
+    angular.module('Beneficiery', []);
+}());
+
+
+// dependencies
+require('./Beneficiery.Request.Controller.js');
+require('./Beneficiery.SelfImprovement.Controller.js');
