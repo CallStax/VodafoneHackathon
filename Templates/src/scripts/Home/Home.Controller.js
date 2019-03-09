49b1408b377ce9b01413dd1cd3b6330df42ab921
Ryan Falzon <musicfreakzryan@hotmail.com>
2019-03-09 08:15:22 +0100

    Added web3 with angularjs

--- Templates/src/scripts/Home/Home.Controller.js
+++ Templates/src/scripts/Home/Home.Controller.js
@@ -7,6 +7,11 @@
 
     function HomeController($scope){
         $scope.Home = '';
+
+        $scope.Web3 = new Web3(
+            new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+        );
+        console.log($scope.Web3.eth.accounts);
     }
 
 }());
