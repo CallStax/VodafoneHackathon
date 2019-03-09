9e79b95d76c92478a198c08c4b1aba7bbbb756c6
Ryan Falzon <musicfreakzryan@hotmail.com>
2019-03-09 15:58:13 +0100

    Finished work on Beneficiery views and controllers for web application

--- Templates/src/scripts/Beneficiery/Beneficiery.Request.Controller.js
+++ Templates/src/scripts/Beneficiery/Beneficiery.Request.Controller.js
@@ -1,17 +1,24 @@
-(function(){
+(function () {
     'use strict';
 
     angular.module('Beneficiery').controller('RequestController', RequestController);
 
     RequestController.$inject = ['$scope'];
 
-    function RequestController($scope){
-        $scope.Home = '';
+    function RequestController($scope) {
+        $scope.UserRequests = [
+            "Supermarket",
+            "Furniture",
+            "Clothes",
+            "Shelter"
+        ];
+        $scope.ChosenUserRequests = {
+            selected: {}
+        };
 
-        $scope.Web3 = new Web3(
-            new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-        );
-        console.log($scope.Web3.eth.accounts);
-    }
+        $scope.SubmitForm = function () {
+            console.log($scope.ChosenUserRequests);
+        };
+    };
 
 }());
