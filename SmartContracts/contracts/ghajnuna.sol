cafa814c7454f4d46972cdfc87f2bdac4367bc11
Ryan Falzon <musicfreakzryan@hotmail.com>
2019-03-09 20:43:00 +0100

    updated smart contract and tests

--- SmartContracts/contracts/ghajnuna.sol
+++ SmartContracts/contracts/ghajnuna.sol
@@ -7,24 +7,27 @@ contract Ghajnuna{
         string name;
         string voNumber;
         bool isSet;
+        bool isApproved;
         bool isSysAdmin;
     }
     
     struct Industry {
+        bytes32 id;
         string code;
         string description;
         bool isSet;
     }
 
     struct Beneficiary {
-        Industry[] industries;
+        bytes32[] industries;
         bool isSet;
         bool isApproved; //TODO: Replaced by Approved By
+        address[] approvedBy;
     }
     
     struct Benefactor {
         string name;
-        Industry[] industries;
+        bytes32[] industries;
         bool isSet;
     }
     
@@ -33,6 +36,10 @@ contract Ghajnuna{
     //NGOs
     address[] private allNgos;
     mapping(address => NGO) private ngos;
+
+    //Industries
+    bytes32[] private allIndustries;
+    mapping(bytes32 => Industry) private industries;
     
     //Beneficiaries
     address[] private allBeneficiaries;
@@ -42,6 +49,12 @@ contract Ghajnuna{
     address[] private allBenefactors;
     mapping(address => Benefactor) private benefactors;
     
+    //Approved beneficiaries
+    //mapping(address(NGO) => address[](beneficiaries))
+    mapping(address => address[]) private beneficiariesApprovedByNgo; //List of NGOs that have onboarded the Beneficiary
+    //mapping(address(Beneficiary) => address[](NGOs))
+    mapping(address => address[]) private ngoApprovalForBeneficiary; //List of NGOs that have onboarded the Beneficiary
+
     //TODO Events
     
     constructor(string memory ngoName, string memory voNumber) public {
@@ -49,18 +62,23 @@ contract Ghajnuna{
         ngos[msg.sender].name = ngoName;
         ngos[msg.sender].voNumber = voNumber;
         ngos[msg.sender].isSet = true;
+        ngos[msg.sender].isApproved = true;
         ngos[msg.sender].isSysAdmin = true;
 
         allNgos.push(msg.sender);
     }
     
-    // Function to get all store managers
-    function registerNGO(address ngoAddress, string memory ngoName, string memory voNumber) public requireAdministrativeNGO {
+    // Functions
+    function requestNGO(address ngoAddress, string memory ngoName, string memory voNumber) public requireAdministrativeNGO {
         ngos[ngoAddress].name = ngoName;
         ngos[ngoAddress].voNumber = voNumber;
         ngos[ngoAddress].isSet = true;
+        ngos[ngoAddress].isApproved = false;
         ngos[ngoAddress].isSysAdmin = false;
-        
+    }
+
+    function approveNGO(address ngoAddress) public requireAdministrativeNGO {
+        ngos[ngoAddress].isApproved = true;
         allNgos.push(ngoAddress);
     }
     
@@ -68,9 +86,65 @@ contract Ghajnuna{
         ngos[ngoAddress].isSysAdmin = true;
     }
 	
-	function getNGO(address ngoAddress) view public returns(string memory, string memory, bool, bool) {
-		return (ngos[ngoAddress].name, ngos[ngoAddress].voNumber, ngos[ngoAddress].isSet, ngos[ngoAddress].isSysAdmin);
+	function getNGO(address ngoAddress) view public returns(string memory, string memory, bool, bool, bool) {
+		return (ngos[ngoAddress].name, ngos[ngoAddress].voNumber, ngos[ngoAddress].isSet, ngos[ngoAddress].isApproved, ngos[ngoAddress].isSysAdmin);
 	}
+
+    function registerIndustry(string memory code, string memory description) public requireAdministrativeNGO {
+        bytes32 id = keccak256(abi.encodePacked(msg.sender, code, description));
+
+        industries[id].id = id;
+        industries[id].code = code;
+        industries[id].description = description;
+        industries[id].isSet = true;
+        
+        allIndustries.push(id);
+    }
+
+    function getIndustry(bytes32 industryId) view public returns(bytes32, string memory, string memory, bool) {
+        return (industries[industryId].id, industries[industryId].code, industries[industryId].description, industries[industryId].isSet);
+    }
+
+    function getAllIndustries() view public returns(bytes32[] memory) {
+        return allIndustries;
+    }
+
+    function registerBeneficiary(address beneficieryAddress, bytes32[] memory chosenIndustries) public {
+        beneficiaries[beneficieryAddress].industries = chosenIndustries;
+        beneficiaries[beneficieryAddress].isSet = true;
+        beneficiaries[beneficieryAddress].isApproved = false;
+    }
+
+    function approveBeneficiary(address beneficiaryAddress) public requireAdministrativeNGO {
+        beneficiaries[beneficiaryAddress].isApproved = true;
+        allBeneficiaries.push(beneficiaryAddress);
+        beneficiariesApprovedByNgo[msg.sender].push(beneficiaryAddress);
+        ngoApprovalForBeneficiary[beneficiaryAddress].push(msg.sender);
+    }
+
+    function getBeneficiary(address beneficiaryAddress) view public returns(bytes32[] memory, bool, bool) {
+        return (beneficiaries[beneficiaryAddress].industries, beneficiaries[beneficiaryAddress].isSet, beneficiaries[beneficiaryAddress].isApproved);
+    }
+
+    function getBeneficiariesApprovedByNGO(address ngo) view public returns(address[] memory) {
+        return beneficiariesApprovedByNgo[ngo];
+    }
+
+    function getNGOApprovalsForBeneficiaries(address beneficiary) view public returns(address[] memory) {
+        return ngoApprovalForBeneficiary[beneficiary];
+    }
+
+    function registerBenefactor(address benefactorAddress, string memory name, bytes32[] memory chosenIndustries) public {
+        benefactors[benefactorAddress].name = name;
+        benefactors[benefactorAddress].industries = chosenIndustries;
+        benefactors[benefactorAddress].isSet = true;
+        
+        allBenefactors.push(benefactorAddress);
+    }
+
+    function getBenefactor(address benefactorAddress) view public returns(string memory, bytes32[] memory, bool) {
+        return (benefactors[benefactorAddress].name, benefactors[benefactorAddress].industries, benefactors[benefactorAddress].isSet);
+    }
     
     //Modifiers
     modifier requireNGO(){
--- SmartContracts/test/Ghajnuna.test.js
+++ SmartContracts/test/Ghajnuna.test.js
@@ -2,84 +2,225 @@ const assert = require('chai').assert;
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const options = { gasLimit: 8000000 };
-
-const web3 = new Web3(ganache.provider(options));
+const web3 = new Web3(ganache.provider());
 const { abi, evm } = require('../compile');
 
 let accounts;
 let ghajnuna;
 
 beforeEach(async () => {
-  // Get a list of all accounts
-  accounts = await web3.eth.getAccounts();
+	// Get a list of all accounts
+	accounts = await web3.eth.getAccounts();
 
-    
-  
-  //Use one of those accounts to deploy
-  //the contract
-  ghajnuna = await new web3.eth.Contract(abi)
-    .deploy({
-      data: evm.bytecode.object,
-      arguments: ['Vodafone Malta Foundation', 'VO/0537']
-    })
-    .send({ from: accounts[0], gas: '1000000' });
+
+
+	//Use one of those accounts to deploy
+	//the contract
+	ghajnuna = await new web3.eth.Contract(abi)
+		.deploy({
+			data: evm.bytecode.object,
+			arguments: ['Vodafone Malta Foundation', 'VO/0537']
+		})
+		.send({ from: accounts[0], gas: 3000000 });
 });
 
 describe('Ghajnuna - Setup', () => {
-  it('deploys a contract', () => {
-    assert.ok(ghajnuna.options.address);
-  });
+	it('deploys a contract', () => {
+		assert.ok(ghajnuna.options.address);
+	});
 
-  it('sets contract creator as ngo', async () => {
-    const ngo = await ghajnuna.methods.getNGO(accounts[0]).call();
-    
-	assert.equal(ngo[0], 'Vodafone Malta Foundation'); //Name
-	assert.equal(ngo[1], 'VO/0537'); //VO Number
-	assert.equal(ngo[2], true); //Is Set
-	assert.equal(ngo[3], true); //Is Sys Admin
-  });
-  
+	it('sets contract creator as ngo', async () => {
+		const ngo = await ghajnuna.methods.getNGO(accounts[0]).call({ from: accounts[0], gas: 3000000 });
+
+		assert.equal(ngo[0], 'Vodafone Malta Foundation'); //Name
+		assert.equal(ngo[1], 'VO/0537'); //VO Number
+		assert.equal(ngo[2], true); //Is Set
+		assert.equal(ngo[3], true);	//Is Approved
+		assert.equal(ngo[4], true); //Is Sys Admin
+	});
+
 });
 
 describe('Ghajnuna - NGO Admin', () => {
 
-  it('allows admin ngo to register new ngo', async () => {
-    await ghajnuna.methods.registerNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: '1000000' });
-	
-	const ngo = await ghajnuna.methods.getNGO(accounts[1]).call();
-	assert.equal(ngo[0], 'Test NGO');
-	assert.equal(ngo[1], 'VO/9999');
-	assert.equal(ngo[2], true); //Is Set
-	assert.equal(ngo[3], false); //is Sys Admin
-  });
-  
-  it('allows admin ngo to set non-admin ngo as admin', async () => {
-	await ghajnuna.methods.registerNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: '1000000' });
-	
-    await ghajnuna.methods.makeNGOAdmin(accounts[1]).send({ from: accounts[0], gas: '1000000' });
-	
-	const ngo = await ghajnuna.methods.getNGO(accounts[1]).call();
+	it('allows admin ngo to register new ngo', async () => {
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
 
-	//perform sanity checks
-	assert.equal(ngo[0], 'Test NGO');
-	assert.equal(ngo[1], 'VO/9999');
-	assert.equal(ngo[2], true); //Is Set
-	assert.equal(ngo[3], true); //Is Sys Admin
-  });
- 
-  it('prevents non-admin ngo to set non-admin ngo as admin', async () => {
+		const ngo = await ghajnuna.methods.getNGO(accounts[1]).call();
+		assert.equal(ngo[0], 'Test NGO');
+		assert.equal(ngo[1], 'VO/9999');
+		assert.equal(ngo[2], true); //Is Set
+		assert.equal(ngo[3], false);	//Is Approved
+		assert.equal(ngo[4], false); //Is Sys Admin
+	});
+
+	it('allows admin ngo to approve an ngo', async () => {
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
+
+		await ghajnuna.methods.approveNGO(accounts[1]).send({ from: accounts[0], gas: 3000000 });
+
+		const ngo = await ghajnuna.methods.getNGO(accounts[1]).call({ from: accounts[0], gas: 3000000 });
+
+		//perform sanity checks
+		assert.equal(ngo[0], 'Test NGO');
+		assert.equal(ngo[1], 'VO/9999');
+		assert.equal(ngo[2], true); //Is Set
+		assert.equal(ngo[3], true);	//Is Approved
+		assert.equal(ngo[4], false); //Is Sys Admin
+	});
+
+	it('allows admin ngo to set non-admin ngo as admin', async () => {
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
+
+		await ghajnuna.methods.makeNGOAdmin(accounts[1]).send({ from: accounts[0], gas: 3000000 });
+
+		const ngo = await ghajnuna.methods.getNGO(accounts[1]).call({ from: accounts[0], gas: 3000000 });
+
+		//perform sanity checks
+		assert.equal(ngo[0], 'Test NGO');
+		assert.equal(ngo[1], 'VO/9999');
+		assert.equal(ngo[2], true); //Is Set
+		assert.equal(ngo[3], false);	//Is Approved
+		assert.equal(ngo[4], true); //Is Sys Admin
+	});
+
+	it('prevents non-admin ngo to approve an ngo', async () => {
 		//Register account[1] as non admin NGO
-		await ghajnuna.methods.registerNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: '1000000' });
-	  
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
+
 		try {
-			await ghajnuna.methods.makeNGOAdmin(accounts[1]).send({ from: accounts[1], gas: '1000000' });
-			
+			await ghajnuna.methods.approveNGO(accounts[1]).send({ from: accounts[1], gas: 3000000 });
+
 			//If we get to here, it means the call succeeded which is wrong!
-			assert.fail("makeNGOAdmin by a non admin NGO succeeded");
-		} catch(error){
+			assert.fail("approveNGO by a non admin NGO succeeded");
+		} catch (error) {
 			assert.include(error.message, 'Only Admin NGOs are able To run this function');
 		}
-  });
-  
+	});
+
+	it('prevents non-admin ngo to set non-admin ngo as admin', async () => {
+		//Register account[1] as non admin NGO
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
+
+		try {
+			await ghajnuna.methods.makeNGOAdmin(accounts[1]).send({ from: accounts[1], gas: 3000000 });
+
+			//If we get to here, it means the call succeeded which is wrong!
+			assert.fail("makeNGOAdmin by a non admin NGO succeeded");
+		} catch (error) {
+			assert.include(error.message, 'Only Admin NGOs are able To run this function');
+		}
+	});
+});
+
+describe('Ghajnuna - Industries', () => {
+
+	it('allows admin ngo to register new industry', async () => {
+		await ghajnuna.methods.registerIndustry('Test Industry', 'Test Description').send({ from: accounts[0], gas: 3000000 });
+
+		const allIndustries = await ghajnuna.methods.getAllIndustries().call();
+		const industry = await ghajnuna.methods.getIndustry(allIndustries[0]).call();
+		assert.equal(industry[1], 'Test Industry');
+		assert.equal(industry[2], 'Test Description');
+		assert.equal(industry[3], true); //Is Set
+	});
+
+	it('prevents benefactor to register new industry', async () => {
+		//Register account[2] as non Benefactor
+		await ghajnuna.methods.registerBenefactor(accounts[2], 'Test Name', []).send({ from: accounts[2], gas: 3000000 });
+
+		try {
+			await ghajnuna.methods.registerIndustry('Test Industry', 'Test Description').send({ from: accounts[2], gas: 3000000 });
+
+			//If we get to here, it means the call succeeded which is wrong!
+			assert.fail("createIndustry by a non admin NGO succeeded");
+		} catch (error) {
+			assert.include(error.message, 'Only Admin NGOs are able To run this function');
+		}
+	});
+
+	it('prevents non-admin ngo to register new industry', async () => {
+		//Register account[1] as non admin NGO
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
+
+		try {
+			await ghajnuna.methods.registerIndustry('Test Industry', 'Test Description').send({ from: accounts[1], gas: 3000000 });
+
+			//If we get to here, it means the call succeeded which is wrong!
+			assert.fail("createIndustry by a non admin NGO succeeded");
+		} catch (error) {
+			assert.include(error.message, 'Only Admin NGOs are able To run this function');
+		}
+	});
+
+	it('prevents beneficiary to register new industry', async () => {
+		//Register account[3] as beneficiary
+		await ghajnuna.methods.registerBeneficiary(accounts[3], []).send({ from: accounts[3], gas: 3000000 });
+
+		try {
+			await ghajnuna.methods.registerIndustry('Test Industry', 'Test Description').send({ from: accounts[3], gas: 3000000 });
+
+			//If we get to here, it means the call succeeded which is wrong!
+			assert.fail("createIndustry by a beneficiary succeeded");
+		} catch (error) {
+			assert.include(error.message, 'Only Admin NGOs are able To run this function');
+		}
+	});
+
+});
+
+describe('Ghajnuna - Benefactor', () => {
+
+	it('allows benefactors to register', async () => {
+		await ghajnuna.methods.registerBenefactor(accounts[2], 'Test Name', []).send({ from: accounts[2], gas: 3000000 });
+
+		const benefactors = await ghajnuna.methods.getBenefactor(accounts[2]).call();
+		assert.equal(benefactors[0], 'Test Name');
+		assert.deepEqual(benefactors[1], []);
+		assert.equal(benefactors[2], true); //Is Set
+	});
+});
+
+describe('Ghajnuna - Beneficiary', () => {
+
+	it('allows beneficiary to register', async () => {
+		await ghajnuna.methods.registerBeneficiary(accounts[3], []).send({ from: accounts[3], gas: 3000000 });
+
+		const beneficiary = await ghajnuna.methods.getBeneficiary(accounts[3]).call();
+		assert.deepEqual(beneficiary[0], []);
+		assert.equal(beneficiary[1], true); //Is Set
+		assert.equal(beneficiary[2], false); //Is Sys Admin
+	});
+
+	it('allows admin ngo to approve beneficiary', async () => {
+		await ghajnuna.methods.registerBeneficiary(accounts[4], []).send({ from: accounts[4], gas: 3000000 });
+
+		await ghajnuna.methods.approveBeneficiary(accounts[4]).send({ from: accounts[0], gas: 3000000 });
+
+		const beneficiary = await ghajnuna.methods.getBeneficiary(accounts[4]).call({ from: accounts[0], gas: 3000000 });
+		const beneficiariesApprovedByNGO = await ghajnuna.methods.getBeneficiariesApprovedByNGO(accounts[0]).call({ from: accounts[0], gas: 3000000 });
+		const ngoApprovalsForBeneficiaries = await ghajnuna.methods.getNGOApprovalsForBeneficiaries(accounts[4]).call({ from: accounts[0], gas: 3000000 });
+
+		//perform sanity checks
+		assert.deepEqual(beneficiary[0], []);
+		assert.equal(beneficiary[1], true); //Is Set
+		assert.equal(beneficiary[2], true); //Is Sys Admin
+		assert.equal(beneficiariesApprovedByNGO.length, 1);
+		assert.equal(ngoApprovalsForBeneficiaries.length, 1);
+	});
+
+	it('prevents non-admin ngo to set non-admin ngo as admin', async () => {
+		//Register account[1] as non admin NGO
+		await ghajnuna.methods.requestNGO(accounts[1], 'Test NGO', 'VO/9999').send({ from: accounts[0], gas: 3000000 });
+
+		try {
+			await ghajnuna.methods.makeNGOAdmin(accounts[1]).send({ from: accounts[1], gas: 3000000 });
+
+			//If we get to here, it means the call succeeded which is wrong!
+			assert.fail("makeNGOAdmin by a non admin NGO succeeded");
+		} catch (error) {
+			assert.include(error.message, 'Only Admin NGOs are able To run this function');
+		}
+	});
+
 });
