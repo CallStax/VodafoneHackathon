246864aa7a23a014ff3048d51295817cf57348a3
scermat <matthew.scerri@gmail.com>
2019-03-10 04:04:14 +0100

    styling changes

--- app/src/scripts/Beneficiary/Beneficiary.Request.Controller.js
+++ app/src/scripts/Beneficiary/Beneficiary.Request.Controller.js
@@ -19,6 +19,30 @@
         $scope.SubmitForm = function () {
             console.log($scope.ChosenUserRequests);
         };
+		
+        $scope.TextData = 
+            {
+                title: {
+                    en: 'Request Service',
+                    mt: 'Servizzi'
+                },
+                heading: {
+                    en: 'Select the type of help you need',
+                    mt: 'Ghazel it-tip ta ghajnuna li ghandek bzonn'
+                },
+                reset: {
+                    en: 'Reset',
+                    mt: 'Erga Ibda'
+                },
+                submit: {
+                    en: 'Submit',
+                    mt: 'Kompli'
+                },
+                text_placeholder: {
+                    en: 'Please provide further details here...',
+                    mt: 'Jekk jghogbok itfa iktar dettall hawnhekk...'
+                }
+            };
     };
 
 }());
